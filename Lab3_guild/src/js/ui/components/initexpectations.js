ea6b133f287857fbc68e44c9d4cac50587da4106
OttoApocalypse <145100017+OttoHyperion@users.noreply.github.com>
2024-10-12 20:34:26 +0500

    Delete Lab3_guild directory

--- Lab3_guild/src/js/ui/components/initexpectations.js
+++
@@ -1,9 +0,0 @@
-
-import {expectationsTemplate} from "../templates/expectationsTemplate.js";
-import {expectationsData} from "../../mockData/expectationsData.js";
-
-const initexpectations=(expectationsNode)=>{
-    expectationsNode.insertAdjacentHTML("beforeend", expectationsTemplate(expectationsData))
-};
-
-export default initexpectations;
